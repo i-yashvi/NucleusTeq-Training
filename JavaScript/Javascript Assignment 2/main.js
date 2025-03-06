25f5c2473b830e303c0b238372fd13edd7a133c2
Yashvi <yashvi198mudgal@gmail.com>
2025-03-06 19:56:13 +0530

    -

--- JavaScript/Javascript Assignment 2/main.js
+++
@@ -1,4 +0,0 @@
-let startGame = document.getElementById("entry-point");
-let inputName = document.getElementById("name-input");
-let startButton = document.getElementById("startButton");
-let playGame = document.getElementById("game-page");
