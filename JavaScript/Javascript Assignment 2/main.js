fc75244cf821d13a777de1fe50da9b3bc41f973b
Yashvi <yashvi198mudgal@gmail.com>
2025-03-06 19:49:58 +0530

    Suggested changes made

---
+++ JavaScript/Javascript Assignment 2/main.js
@@ -0,0 +1,4 @@
+let startGame = document.getElementById("entry-point");
+let inputName = document.getElementById("name-input");
+let startButton = document.getElementById("startButton");
+let playGame = document.getElementById("game-page");
